9186b8fd372b1e4277d619148f4c734f72a0754e
Sheersha Jain <sheershajain@gmail.com>
2020-04-11 18:44:28 +0530

    Create JULKA.py

---
+++ JULKA.py
@@ -0,0 +1,7 @@
+for _ in range(10):
+    total = int(input())
+    difference = int(input())
+    klaudia = (total + difference) // 2
+    natalia = klaudia - difference
+    print(klaudia)
+    print(natalia)
